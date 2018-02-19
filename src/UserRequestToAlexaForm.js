94972a2f87376f5bbeeb48d1d8ad2e4220f323c4
Mahesh Sabbavarapu <maheshba@amazon.com>
2018-02-18 17:13:51 -0800

    Converted UserRequestToAlexaForm into a functional component because this component is stateless and functional components are considered a better practice in such cases.

--- src/UserRequestToAlexaForm.js
+++ src/UserRequestToAlexaForm.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const DEFAULT_PLACEHOLDER_VALUE = require('./Constants');
 /*
 The input component where user's type in their requests for Alexa
 */
-class UserRequestToAlexaForm extends Component {
-    render() {
-        return (
-            <form onSubmit={(e) => this.props.onSubmit(e)}>
-                <input type="text" value={this.props.value} onChange={(e) => this.props.onChange(e)}
-                    placeholder={DEFAULT_PLACEHOLDER_VALUE}
-                    className="request-input"
-                />
-            </form>
-        );
-    }
+
+const UserRequestToAlexaForm = (props) => {
+    return (
+        <form onSubmit={(e) => props.onSubmit(e)}>
+            <input type="text" value={props.value} onChange={(e) => props.onChange(e)}
+                placeholder={DEFAULT_PLACEHOLDER_VALUE}
+                className="request-input"
+            />
+        </form>
+    );
 }
 
 export default UserRequestToAlexaForm;
