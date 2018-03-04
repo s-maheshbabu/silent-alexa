fe00193edaeb294a852b7e22bad2cc1c00830e01
Mahesh Sabbavarapu <maheshba@amazon.com>
2018-03-04 14:26:58 -0800

    Formatting changes.

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
-import { ChatFeed, Message } from 'react-chat-ui'
+import React, { Component } from "react";
+import { ChatFeed, Message } from "react-chat-ui";
 
-import UserRequestToAlexaForm from './UserRequestToAlexaForm.js';
-const users = require('./ConversingUsers');
+import UserRequestToAlexaForm from "./UserRequestToAlexaForm.js";
+const users = require("./ConversingUsers");
 
 class ChatWindow extends Component {
   constructor() {
     super();
     this.state = {
       messages: [
-        new Message({ id: 0, message: 'Hello Alexa!', senderName: 'You' }),
+        new Message({ id: 0, message: "Hello Alexa!", senderName: "You" }),
         new Message({
           id: 1,
-          message: 'Hey! Alexa here.',
-          senderName: 'Alexa',
-        }),
+          message: "Hey! Alexa here.",
+          senderName: "Alexa"
+        })
       ],
-      userRequestToAlexa: '',
+      userRequestToAlexa: "",
       useCustomBubble: false,
       curr_user: 0,
       is_typing: false
     };
 
-    this.onUserRequestToAlexaSubmit = this.onUserRequestToAlexaSubmit.bind(this);
-    this.handleChangeInUserRequestToAlexa = this.handleChangeInUserRequestToAlexa.bind(this);
+    this.onUserRequestToAlexaSubmit = this.onUserRequestToAlexaSubmit.bind(
+      this
+    );
+    this.handleChangeInUserRequestToAlexa = this.handleChangeInUserRequestToAlexa.bind(
+      this
+    );
   }
 
   /*
@@ -35,11 +39,11 @@ class ChatWindow extends Component {
     e.preventDefault();
     if (!userRequestToAlexa || 0 === userRequestToAlexa.length) {
       console.log("Request string for Alexa was empty: " + userRequestToAlexa);
-      this.setState({ userRequestToAlexa: '' });
+      this.setState({ userRequestToAlexa: "" });
       return false;
     }
     this.pushMessage(this.state.curr_user, userRequestToAlexa);
-    this.setState({ userRequestToAlexa: '' });
+    this.setState({ userRequestToAlexa: "" });
     return true;
   }
 
@@ -57,7 +61,7 @@ class ChatWindow extends Component {
     const newMessage = new Message({
       id: userid,
       message,
-      senderName: users[userid],
+      senderName: users[userid]
     });
     messagesCopy.push(newMessage);
     this.setState({ messages: messagesCopy });
@@ -72,8 +76,7 @@ class ChatWindow extends Component {
 
   render() {
     return (
-      <div className="ChatWindow" >
-
+      <div className="ChatWindow">
         <ChatFeed
           messages={this.state.messages}
           isTyping={this.state.is_typing} // Boolean: is the recipient typing
@@ -84,10 +87,9 @@ class ChatWindow extends Component {
 
         <UserRequestToAlexaForm
           value={this.state.userRequestToAlexa}
-          onChange={(e) => this.handleChangeInUserRequestToAlexa(e)}
-          onSubmit={(e) => this.onUserRequestToAlexaSubmit(e)}
+          onChange={e => this.handleChangeInUserRequestToAlexa(e)}
+          onSubmit={e => this.onUserRequestToAlexaSubmit(e)}
         />
-
       </div>
     );
   }
--- src/ChatWindow.test.js
+++ src/ChatWindow.test.js
@@ -1,114 +1,132 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 
-import { Message } from 'react-chat-ui';
-import ChatWindow from './ChatWindow';
+import { Message } from "react-chat-ui";
+import ChatWindow from "./ChatWindow";
 
-const users = require('./ConversingUsers');
+const users = require("./ConversingUsers");
 
 let chatWindow;
 let chatWindowInstance;
 let originalState;
 beforeEach(() => {
-    chatWindow = shallow(<ChatWindow />);
-    chatWindowInstance = chatWindow.instance();
-    originalState = JSON.parse(JSON.stringify(chatWindow.instance().state));
+  chatWindow = shallow(<ChatWindow />);
+  chatWindowInstance = chatWindow.instance();
+  originalState = JSON.parse(JSON.stringify(chatWindow.instance().state));
 });
 
-it('renders ChatWindow without crashing', () => {
-    mount(<ChatWindow />);
+it("renders ChatWindow without crashing", () => {
+  mount(<ChatWindow />);
 });
 
-it('pushMessage persists a given message in state', () => {
-    const numberOfMessagesAlreadyInState = originalState.messages.length;
-    expect(originalState.messages.length).toBe(numberOfMessagesAlreadyInState);
+it("pushMessage persists a given message in state", () => {
+  const numberOfMessagesAlreadyInState = originalState.messages.length;
+  expect(originalState.messages.length).toBe(numberOfMessagesAlreadyInState);
 
-    const userid = 1;
-    const message = 'test message';
-    const expectedMessage = new Message({ id: userid, message, senderName: users[userid] });
+  const userid = 1;
+  const message = "test message";
+  const expectedMessage = new Message({
+    id: userid,
+    message,
+    senderName: users[userid]
+  });
 
-    chatWindow.instance().pushMessage(userid, message);
-    const finalState = chatWindow.instance().state;
+  chatWindow.instance().pushMessage(userid, message);
+  const finalState = chatWindow.instance().state;
 
-    const finalMessages = finalState.messages;
-    expect(finalMessages.length).toBe(numberOfMessagesAlreadyInState + 1);
-    expect(finalMessages[numberOfMessagesAlreadyInState]).toEqual(expectedMessage);
+  const finalMessages = finalState.messages;
+  expect(finalMessages.length).toBe(numberOfMessagesAlreadyInState + 1);
+  expect(finalMessages[numberOfMessagesAlreadyInState]).toEqual(
+    expectedMessage
+  );
 });
 
-it('handles gracefully when pushMessage is called with an unknown user', () => {
-    const invalidUserid = 1000; // valid values are just 0 and 1
+it("handles gracefully when pushMessage is called with an unknown user", () => {
+  const invalidUserid = 1000; // valid values are just 0 and 1
 
-    chatWindow.instance().pushMessage(invalidUserid, 'test message');
-    const finalState = chatWindow.instance().state;
+  chatWindow.instance().pushMessage(invalidUserid, "test message");
+  const finalState = chatWindow.instance().state;
 
-    // Nothing about the state should have changed.
-    expect(finalState).toEqual(originalState);
+  // Nothing about the state should have changed.
+  expect(finalState).toEqual(originalState);
 });
 
-it('handles gracefully when pushMessage is called with an empty or null message', () => {
-    const userid = 1;
-    const emptyMessage = '';
+it("handles gracefully when pushMessage is called with an empty or null message", () => {
+  const userid = 1;
+  const emptyMessage = "";
 
-    chatWindow.instance().pushMessage(userid, emptyMessage);
-    let finalState = chatWindow.instance().state;
+  chatWindow.instance().pushMessage(userid, emptyMessage);
+  let finalState = chatWindow.instance().state;
 
-    // Nothing about the state should have changed.
-    expect(finalState).toEqual(originalState);
+  // Nothing about the state should have changed.
+  expect(finalState).toEqual(originalState);
 
-    let nullMessage;
+  let nullMessage;
 
-    chatWindow.instance().pushMessage(userid, nullMessage);
-    finalState = chatWindow.instance().state;
+  chatWindow.instance().pushMessage(userid, nullMessage);
+  finalState = chatWindow.instance().state;
 
-    // Nothing about the state should have changed.
-    expect(finalState).toEqual(originalState);
+  // Nothing about the state should have changed.
+  expect(finalState).toEqual(originalState);
 });
 
-it('handles the user\'s form submission with request to Alexa properly', () => {
-    const chatWindow = mount(<ChatWindow />);
-    const chatWindowInstance = chatWindow.instance();
-    const originalState = JSON.parse(JSON.stringify(chatWindowInstance.state));
+it("handles the user's form submission with request to Alexa properly", () => {
+  const chatWindow = mount(<ChatWindow />);
+  const chatWindowInstance = chatWindow.instance();
+  const originalState = JSON.parse(JSON.stringify(chatWindowInstance.state));
 
-    const userRequestToAlexaForm = chatWindow.find('form').get(0);
-    const numberOfMessagesAlreadyInState = originalState.messages.length;
+  const userRequestToAlexaForm = chatWindow.find("form").get(0);
+  const numberOfMessagesAlreadyInState = originalState.messages.length;
 
-    const mockuserRequestToAlexa = 'mock request';
-    const userid = 1;
-    chatWindowInstance.setState({
-        userRequestToAlexa: mockuserRequestToAlexa,
-        curr_user: userid
-    });
+  const mockuserRequestToAlexa = "mock request";
+  const userid = 1;
+  chatWindowInstance.setState({
+    userRequestToAlexa: mockuserRequestToAlexa,
+    curr_user: userid
+  });
 
-    const preventDefaultSpy = jest.fn();
-    chatWindow.find('form').simulate('submit', { preventDefault: preventDefaultSpy });
+  const preventDefaultSpy = jest.fn();
+  chatWindow
+    .find("form")
+    .simulate("submit", { preventDefault: preventDefaultSpy });
 
-    // Verify that preventDefault() is being called.
-    expect(preventDefaultSpy.mock.calls.length).toBe(1);
+  // Verify that preventDefault() is being called.
+  expect(preventDefaultSpy.mock.calls.length).toBe(1);
 
-    const finalState = chatWindowInstance.state;
+  const finalState = chatWindowInstance.state;
 
-    const expectedMessage = new Message({ id: userid, message: mockuserRequestToAlexa, senderName: users[userid] });
-    const finalMessages = finalState.messages;
-    expect(finalMessages.length).toBe(numberOfMessagesAlreadyInState + 1);
-    expect(finalMessages[numberOfMessagesAlreadyInState]).toEqual(expectedMessage);
+  const expectedMessage = new Message({
+    id: userid,
+    message: mockuserRequestToAlexa,
+    senderName: users[userid]
+  });
+  const finalMessages = finalState.messages;
+  expect(finalMessages.length).toBe(numberOfMessagesAlreadyInState + 1);
+  expect(finalMessages[numberOfMessagesAlreadyInState]).toEqual(
+    expectedMessage
+  );
 
-    expect(chatWindowInstance.state.userRequestToAlexa).toEqual('');
+  expect(chatWindowInstance.state.userRequestToAlexa).toEqual("");
 });
 
-it('handles gracefully when the input form is submitted with a null or empty request string', () => {
-    const numberOfMessagesAlreadyInState = originalState.messages.length;
+it("handles gracefully when the input form is submitted with a null or empty request string", () => {
+  const numberOfMessagesAlreadyInState = originalState.messages.length;
 
-    const userRequestToAlexaForm = chatWindow.find('UserRequestToAlexaForm').get(0);
+  const userRequestToAlexaForm = chatWindow
+    .find("UserRequestToAlexaForm")
+    .get(0);
 
-    let nulluserRequestToAlexa;
-    chatWindowInstance.setState({
-        userRequestToAlexa: nulluserRequestToAlexa
-    });
+  let nulluserRequestToAlexa;
+  chatWindowInstance.setState({
+    userRequestToAlexa: nulluserRequestToAlexa
+  });
 
-    chatWindow.find('UserRequestToAlexaForm').simulate('submit', { preventDefault: jest.fn() });
+  chatWindow
+    .find("UserRequestToAlexaForm")
+    .simulate("submit", { preventDefault: jest.fn() });
 
-    const finalState = chatWindowInstance.state;
+  const finalState = chatWindowInstance.state;
 
-    // Nothing about the state should have changed.
-    expect(finalState).toEqual(originalState);
+  // Nothing about the state should have changed.
+  expect(finalState).toEqual(originalState);
 });
