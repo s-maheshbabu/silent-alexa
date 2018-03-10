deb88329bbc6e50fd69a60a05260c73b09284263
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-03-09 23:15:12 -0800

    AVS returns HTTP/2 Multipart messages to it clients. See https://developer.amazon.com/docs/alexa-voice-service/structure-http2-request.html for more details on the exact structure.
    Added a parser that takes the multi-part reponse and extracts the 'caption' which represents the actual content that should be displayed to the user as Alexa's response.
    
    The rest of the message contains audio bytes of the same response, SSML tagsetc., all of which gets dropped.
    
    We are using https://github.com/miguelmota/http-message-parser/ to do the parsing.
    
    Unit tests added.
    
    Note: http-message-parser has a bug in the latest version and so package.json is configured to not use the latest version of this package but use 0.0.22.
    A bug has been filed against the developer at https://github.com/miguelmota/http-message-parser/issues/4

---
+++ src/SpeakDirectiveParser.js
@@ -0,0 +1,56 @@
+const httpMessageParser = require("http-message-parser");
+const { hasIn } = require("immutable");
+const util = require("util");
+
+import IllegalArgumentError from "./errors/IllegalArgumentError";
+
+/**
+ * This method parses the multi-part AVS responses and extracts the
+ * string representing Alexa's response. It strips away other information
+ * like binary audio data, ssml tags etc.
+ *
+ * @param {String} alexaRawResponse The multi-part response from AVS. This
+ * should not be empty or undefined.
+ */
+export function extractAlexaTextResponse(alexaRawResponse) {
+  if (!alexaRawResponse) {
+    throw new IllegalArgumentError(
+      "The response to be parsed cannot be empty. Input: " + alexaRawResponse
+    );
+  }
+
+  const parsedResponse = httpMessageParser(alexaRawResponse);
+  if (
+    !parsedResponse ||
+    !parsedResponse.multipart ||
+    !parsedResponse.multipart[0].body ||
+    parsedResponse.multipart[0].body.length == 0
+  ) {
+    throw new IllegalArgumentError(
+      "Given response is not a valid multi-part message. Input: " +
+        alexaRawResponse
+    );
+  }
+
+  const avsDirectiveBuffer = parsedResponse.multipart[0].body;
+  let avsDirective;
+  try {
+    avsDirective = JSON.parse(avsDirectiveBuffer);
+  } catch (e) {
+    console.error(
+      "JSON parse error: " + util.inspect(e, { showHidden: true, depth: null })
+    );
+    throw new IllegalArgumentError(
+      "Given directive couldn't be parsed to a JSON object. Input: " +
+        avsDirectiveBuffer.toString()
+    );
+  }
+
+  if (!hasIn(avsDirective, ["directive", "payload", "caption"]))
+    throw new IllegalArgumentError(
+      "Given directive doesn't contain the expected path directive.payload.caption. Input: " +
+        avsDirective
+    );
+
+  return avsDirective.directive.payload.caption;
+}
