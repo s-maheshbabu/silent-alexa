130454d12afe8496d4ba233072c1b7997499bec6
Manasa Gootla <manugootla@gmail.com>
2018-03-10 18:38:28 -0800

    Integrate LoginWithAmazon with App to provide user authentication information which is used to communicate with Alexa

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,36 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import App from "./App";
+
+let app;
+let appInstance;
+let originalState;
+
+beforeEach(() => {
+     app = shallow(<App />);
+     appInstance = app.instance();
+     originalState = JSON.parse(JSON.stringify(app.instance().state));
+});
+
 it("renders without crashing", () => {
   shallow(<App />);
 });
+
 it("snapshot testing that it renders correctly", () => {
   const wrapper = mount(<App />);
   expect(wrapper).toMatchSnapshot();
+});
+
+it("should not change state when authorization response (implicit grant) is not defined", () => {
+  appInstance.handleAuthenticationInfoUpdate();
+  expect(appInstance.state).toEqual(originalState);
+});
+
+it("should change state when authorization response (implicit grant) is defined", () => {
+  const authResponse = {access_token: "some_access_token", expires_in: 30, state: "user_defined_state"};
+  appInstance.handleAuthenticationInfoUpdate(authResponse);
+  const finalState = {
+    authenticationInfo: {access_token: authResponse.access_token, expires_in: authResponse.expires_in}
+  };
+  expect(appInstance.state).toEqual(finalState);
 });
--- src/LoginWithAmazon.test.js
+++ src/LoginWithAmazon.test.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
-import LoginWithAmazon from './LoginWithAmazon';
-const util = require('util')
+import React from "react";
+import {shallow, mount} from "enzyme";
+import LoginWithAmazon from "./LoginWithAmazon";
+const util = require("util")
 
 let loginWithAmazon;
 let loginWithAmazonInstance;
-let mockPopulateAuthenticationInfo;
+let mockUpdateAuthenticationInfo;
 let amazonAuthorizationSpy;
 
 beforeEach(() => {
-    mockPopulateAuthenticationInfo = jest.fn();
-    loginWithAmazon = mount(<LoginWithAmazon populateAuthenticationInfo={mockPopulateAuthenticationInfo}/>);
+    mockUpdateAuthenticationInfo = jest.fn();
+    loginWithAmazon = shallow(<LoginWithAmazon updateAuthenticationInfo={mockUpdateAuthenticationInfo}/>);
     loginWithAmazonInstance = loginWithAmazon.instance();
     amazonAuthorizationSpy = jest.fn();
 
-    Object.defineProperty(window, 'amazon', {
+    Object.defineProperty(window, "amazon", {
         value: { Login: { authorize: amazonAuthorizationSpy } },
         writable: true
     });
 });
 
-afterEach(() => {
-    loginWithAmazon.unmount();
+it("renders without crashing", () => {
+    shallow(<LoginWithAmazon />);
 });
 
-it('renders without crashing', () => {
-    const wrapper = shallow(<LoginWithAmazon />);
-    wrapper.unmount();
+it("renders correctly (snapshot testing)", () => {
+  const wrapper = mount(<LoginWithAmazon />);
+  expect(wrapper).toMatchSnapshot();
+
+  wrapper.unmount();
 });
 
-it('verify amazon authorization called when login button is clicked', () => {
-    mount(<LoginWithAmazon />).find('button').simulate('click');
+it("verify amazon authorization called when login button is clicked", () => {
+    mount(<LoginWithAmazon />).find("button").simulate("click");
 
     // Verify authorize called once
     expect(amazonAuthorizationSpy.mock.calls.length).toBe(1);
 });
 
-it('logs error message when response is undefined', () => {
+it("logs error message when response is undefined", () => {
     global.console = {
       log: jest.fn()
     }
@@ -45,30 +47,30 @@ it('logs error message when response is undefined', () => {
     loginWithAmazonInstance.handleResponse(response)
 
     // Verify error message has been logged to console
-    expect(global.console.log).toHaveBeenCalledWith('Encountered an error on login: undefined')
+    expect(global.console.log).toHaveBeenCalledWith("Encountered an error on login: undefined")
 });
 
-it('logs error message when authorization request encounters an error', () => {
+it("logs error message when authorization request encounters an error", () => {
     global.console = {
       log: jest.fn()
     };
 
     const response = {
-      error: 'some_error_code',
-      error_description: 'description about error as string',
-      state: {page: 'http://somePage'}
+      error: "some_error_code",
+      error_description: "description about error as string",
+      state: {page: "http://somePage"}
     }
     loginWithAmazonInstance.handleResponse(response)
 
     // Verify error message has been logged to console
     expect(global.console.log)
-      .toHaveBeenCalledWith('Encountered an error on login: ' + util.inspect(response, { showHidden: true, depth: null }))
+      .toHaveBeenCalledWith("Encountered an error on login: " + util.inspect(response, { showHidden: true, depth: null }))
 });
 
-it('calls populateAuthenticationInfo when authorization request is success', () => {
-    const response={access_token: 'some_access_token', expires_in: 3600}
+it("calls populateAuthenticationInfo when authorization request is success", () => {
+    const response={access_token: "some_access_token", expires_in: 3600}
     loginWithAmazonInstance.handleResponse(response)
 
     // Verify populateAuthenticationInfo is called with the authorization response argument
-    expect(mockPopulateAuthenticationInfo).toHaveBeenCalledWith(response)
+    expect(mockUpdateAuthenticationInfo).toHaveBeenCalledWith(response)
 });
