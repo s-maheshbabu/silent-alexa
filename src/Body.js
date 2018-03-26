90956322a1c271c8428ab8644448612eb596f4ab
Manasa Gootla <manugootla@gmail.com>
2018-03-25 22:34:47 -0700

    Add keys to the list of components in Body.js to avoid warnings. For more information: https://fb.me/react-warning-keys

--- src/Body.js
+++ src/Body.js
@@ -7,11 +7,11 @@ export default class Body extends Component {
   render() {
     return (
       [
-        <MuiThemeProvider>
+        <MuiThemeProvider key="muiThemeProvider">
           <ChatWindow />
         </MuiThemeProvider>,
 
-        <RightPanel />
+        <RightPanel key="rightPanel" />
       ]
     )
   }
