a827ee8654b7c3be8a3198a246ad61629451a98a
Manasa Gootla <manugootla@gmail.com>
2018-04-01 18:49:46 -0700

    Updated snapshot testing to use shallow wherever necessary to limit the scope to unit testing.

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import Footer from "./Footer";
 
 it("renders Footer without crashing", () => {
-  mount(<Footer />);
-});
-
-it("renders correctly (snapshot testing)", () => {
-  const wrapper = mount(<Footer />);
+  const wrapper = shallow(<Footer />);
   expect(wrapper).toMatchSnapshot();
 
   wrapper.unmount();
