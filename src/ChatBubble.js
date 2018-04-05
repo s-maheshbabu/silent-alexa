0fc84c94ae8860f50309d4e89570d47a7da0ee6d
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-04-05 16:30:48 -0700

    Extract CSS into a separate file.

--- src/ChatBubble.js
+++ src/ChatBubble.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { ChatBubble } from "react-chat-ui";
 
+import styles from "./ChatBubble.css.js";
+
 export default function(props) {
   return (
     <ChatBubble
       message={props.message}
       bubbleStyles={{
-        text: {
-          fontSize: 20
-        },
-        chatbubble: {
-          background: "mediumslateblue"
-        },
-        userBubble: {
-          background: "lightcoral"
-        }
+        ...styles.bubbleStyles
       }}
     />
   );
