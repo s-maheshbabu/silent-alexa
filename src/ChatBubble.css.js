f6e021fe8a9833a5c1d050879452afe10f985580
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-04-07 18:33:11 -0700

    Move the TODO from code to an issue because this is not in our control on when it can be fixed. Issue created: https://github.com/s-maheshbabu/silent-alexa/issues/34
    Once CreateReactApp v2 is available, the issue can be resolved.

--- src/ChatBubble.css.js
+++ src/ChatBubble.css.js
@@ -1,13 +1,3 @@
-/*
-TODO:
-We are using CSS-in-JS style here. Ideally, we should be using CSS Modules.
-Support for CSS Modules is coming in Create React App V2 and once we start
-using it, we should migrate this to CSS modules.
-
-Context:
-https://medium.com/nulogy/how-to-use-css-modules-with-create-react-app-9e44bec2b5c2
-https://github.com/facebook/create-react-app/pull/2285
-*/
 export default {
   bubbleStyles: {
     text: {
