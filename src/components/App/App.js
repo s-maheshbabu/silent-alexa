66c157aa1f375afe9d6bb4dd1d5a5c86211a7b9b
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2019-06-28 16:29:20 -0700

    commit before context/hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import util from "util";
 
 import Header from "Header/Header";
 import Body from "Body/Body";
 import Footer from "Footer/Footer";
 import Routes from "Routes/Routes";
-import AuthenticationInfo from "AuthenticationInfo";
 
 class App extends Component {
   constructor(props) {
@@ -18,65 +16,12 @@ class App extends Component {
   render() {
     return (
       <div id="page">
-        <Routes
-          updateAuthenticationInfo={authenticationInfo =>
-            this.updateAuthenticationInfo(authenticationInfo)
-          }
-        />
-        <Header
-          isAuthenticationInfoValid={() => this.isAuthenticationInfoValid()}
-          clearAuthenticationInfo={() => this.clearAuthenticationInfo()}
-        />
-        <Body
-          authenticationInfo={this.state.authenticationInfo}
-          isAuthenticationInfoValid={() => this.isAuthenticationInfoValid()}
-          clearAuthenticationInfo={() => this.clearAuthenticationInfo()}
-        />
+        <Routes />
+        <Header />
+        <Body />
         <Footer />
       </div>
     );
-  }
-
-  /**
-   * Updates authenticationInfo prop in the component's state
-   * @param authenticationInfo Instance of AuthenticationInfo
-   */
-  updateAuthenticationInfo = function(authenticationInfo) {
-    if (
-      authenticationInfo &&
-      authenticationInfo instanceof AuthenticationInfo
-    ) {
-      this.setState({ authenticationInfo: authenticationInfo });
-    } else {
-      console.log(
-        `Ignoring update. Received invalid authenticationInfo object: ${util.inspect(
-          authenticationInfo,
-          {
-            showHidden: true,
-            depth: null
-          }
-        )}.`
-      );
-    }
-  };
-
-  /**
-   * @returns true if the authenticationInfo is defined and is an instance of {@link AuthenticationInfo}.
-   *          false, otherwise.
-   * TODO: Incorporate {@link  AuthenticationInfo#isValid} functionality when available.
-   */
-  isAuthenticationInfoValid() {
-    return (
-      this.state.authenticationInfo &&
-      this.state.authenticationInfo instanceof AuthenticationInfo
-    );
-  }
-
-  /**
-   * Clears authenticationInfo prop in the component's state
-   */
-  clearAuthenticationInfo() {
-    this.setState({ authenticationInfo: undefined });
   }
 }
 
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,4 @@
 import React from "react";
-import AppBar from "material-ui/AppBar";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { shallow } from "enzyme";
 
 import Header from "./Header";
@@ -9,40 +7,4 @@ it("renders Header without crashing", () => {
   expect(wrapper).toMatchSnapshot();
 
   wrapper.unmount();
-});
-
-it("verifies that props are passed to LoginControl component", () => {
-  const mockIsAuthenticationInfoValid = jest.fn();
-  const mockClearAuthenticationInfo = jest.fn();
-  const header = shallow(
-    <Header
-      isAuthenticationInfoValid={mockIsAuthenticationInfoValid}
-      clearAuthenticationInfo={mockClearAuthenticationInfo}
-    />
-  );
-  const loginControl = header
-    .find(MuiThemeProvider)
-    .find(AppBar)
-    .prop("iconElementRight");
-
-  // Verify that only desired number of props are passed on to LoginControl
-  expect(Object.keys(loginControl.props).length).toBe(2);
-
-  // Verify that LoginControl is passed on isAuthenticationInfoValid prop
-  const isAuthenticationInfoValidProp =
-    loginControl.props["isAuthenticationInfoValid"];
-  expect(isAuthenticationInfoValidProp).toBeDefined();
-
-  // Verify that calling isAuthenticationInfoValid prop function calls the mockIsAuthenticationInfoValid
-  isAuthenticationInfoValidProp();
-  expect(mockIsAuthenticationInfoValid).toHaveBeenCalledTimes(1);
-
-  // Verify that LoginControl is passed on clearAuthenticationInfo prop
-  const clearAuthenticationInfoProp =
-    loginControl.props["clearAuthenticationInfo"];
-  expect(clearAuthenticationInfoProp).toBeDefined();
-
-  // Verify that calling clearAuthenticationInfo prop function calls the mockClearAuthenticationInfo
-  clearAuthenticationInfoProp();
-  expect(mockClearAuthenticationInfo).toHaveBeenCalledTimes(1);
 });
