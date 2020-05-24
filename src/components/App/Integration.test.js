8b03db144607b4ad20f417c5762f9e648a46ed29
Mahesh <s-maheshbabu@users.noreply.github.com>
2020-05-24 00:08:23 -0700

    Introduce integration tests. (#69)
    * Introduce integration tests.
    
    A recent bug proved that unit testing is not adequate. The bug was the
    logout button not working as intended. The button would change to
    'Login' and clear access tokens which is what we unit test for. However,
    because of a bug in a different part in the code, the chat window was
    not disappearing.
    
    This underscored the need for integration tests. react-testing-library
    is a strong proponent of integration tests and provides many utility
    functions to effectively write integration tests.
    
    In this commit, we write a single integration test covering the above described
    scenario using react-testing-library. The idea is to continue adding
    more integration tests going forward.
    
    * Leverage toBeInTheDocument helper method.

---
+++ src/components/App/Integration.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import App from "./App";
+import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from "react-router-dom";
+import '@testing-library/jest-dom/extend-expect';
+import { CookiesProvider, Cookies } from 'react-cookie';
+
+import { AMAZON_LOGIN_COOKIE } from "Constants";
+
+afterEach(cleanup);
+
+it("integration test to verify the actions to be taken when a currently authenticated user clicks on the logout button while on the chat page.", async () => {
+  let cookies = new Cookies();
+  cookies.set(AMAZON_LOGIN_COOKIE, 'access token is present', { path: '/' });
+
+  const { getByText, getByRole, queryByText, queryByRole } = renderWithProviders(<App />, cookies);
+  const logoutButton = getByText("Logout");
+  const chatInputTextBox = getByRole("textbox");
+  expect(logoutButton).toBeInTheDocument();
+  expect(chatInputTextBox).toBeInTheDocument();
+
+  fireEvent.click(logoutButton);
+
+  await waitFor(() => {
+    expect(queryByText("Logout")).not.toBeInTheDocument();
+    expect(queryByRole("textbox")).not.toBeInTheDocument();
+
+    expect(queryByText("Login")).toBeInTheDocument();
+    expect(getByText("WELCOME TO SILENT ALEXA", { exact: false })).toBeInTheDocument();
+  })
+});
+
+function renderWithProviders(
+  component,
+  cookies,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {}
+) {
+  return {
+    ...render(
+      <Router history={history}>
+        <CookiesProvider cookies={cookies}>
+          {component}
+        </CookiesProvider>
+      </Router>),
+    history
+  }
+}
